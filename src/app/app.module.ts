eba6c697eaf1c3428cabf8b4d4fb9ded573f1f23
Saksham <2020UCP1801@mnit.ac.in>
2021-08-23 16:35:06 +0530

    cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './Components/items/items.component';
 import { ItemComponent } from './Components/item/item.component';
+import { CartComponent } from './Components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    ItemComponent
+    ItemComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
