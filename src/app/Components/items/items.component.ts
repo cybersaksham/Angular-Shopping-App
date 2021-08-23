e24600a2a36371c774d1aa8996e04090d4dfb658
Saksham <2020UCP1801@mnit.ac.in>
2021-08-23 12:24:22 +0530

    5 items added

--- src/app/Components/items/items.component.ts
+++ src/app/Components/items/items.component.ts
@@ -1,13 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/Models/item';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent implements OnInit {
 
-  constructor() { }
+export class ItemsComponent implements OnInit {
+  items: Item[];
+
+  constructor() {
+    this.items = [
+      {
+        id: 1,
+        title: "First Item",
+        desc: "This is the first item",
+        price: 40
+      },
+      {
+        id: 2,
+        title: "Second Item",
+        desc: "This is the second item",
+        price: 84
+      },
+      {
+        id: 3,
+        title: "Third Item",
+        desc: "This is the third item",
+        price: 15
+      },
+      {
+        id: 4,
+        title: "Fourth Item",
+        desc: "This is the fourth item",
+        price: 35
+      },
+      {
+        id: 5,
+        title: "Fifth Item",
+        desc: "This is the fifth item",
+        price: 20
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
