2aa632eb0588a2369b1df4034db40d62416afa6e
Saksham <2020UCP1801@mnit.ac.in>
2021-08-23 12:29:13 +0530

    individual item component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './Components/items/items.component';
+import { ItemComponent } from './Components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
