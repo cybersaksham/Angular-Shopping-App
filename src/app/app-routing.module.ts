3cee213ba45969d24e97aeb77f03c8d87459338e
Saksham <2020UCP1801@mnit.ac.in>
2021-08-23 16:36:58 +0530

    routes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './Components/cart/cart.component';
+import { ItemsComponent } from './Components/items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ItemsComponent },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
