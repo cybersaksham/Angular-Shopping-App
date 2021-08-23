b1beaa970f3aa28dd0c2b81e2362fda9b59af4e5
Saksham <2020UCP1801@mnit.ac.in>
2021-08-23 12:32:16 +0530

    items passed

--- src/app/Components/item/item.component.ts
+++ src/app/Components/item/item.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/app/Models/item';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
+
 export class ItemComponent implements OnInit {
+  @Input() item!: Item;
 
   constructor() { }
 
